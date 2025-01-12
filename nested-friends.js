fd9ae2bd56cc6dc17d1cc16bf5e71d82338f2bbe
mahidulkabir <mahidulkabir8@gmail.com>
2025-01-12 10:27:06 +0600

    added firens circle conditions

---
+++ nested-friends.js
@@ -0,0 +1,20 @@
+// tell about me and my friends 
+let myScore = 84;
+let friendsScore = 66 ;
+
+if (myScore <80 ){
+    console.log("Vallge na . Basay jeye ghumabo");
+}else {
+    if (friendsScore >= 80){
+        console.log("Lets go for a lunch");
+    } else if(friendsScore <80 && friendsScore >= 60){
+        console.log("Dost..Bepar na.  good luck next time. ");
+    } else if( friendsScore <60 && friendsScore >= 40 ){
+     console.log("Tor message seen korbona dstoo!! ");
+    }else {
+        console.log("Jah beda ! Block Kha!")
+    }
+
+}
+
+
