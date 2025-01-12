d57c34fbb6b012ee7299b82a3b517bdd9a3397f2
mahidulkabir <mahidulkabir8@gmail.com>
2025-01-12 08:09:04 +0600

    first commit

---
+++ free-drinks.js
@@ -0,0 +1,9 @@
+// free drinks 
+let burgerPrice = 500;
+let cokePrice=30;
+if (burgerPrice >= 500){
+    console.log( "Congratulations! You Get a free Coke ");
+    console.log("coke price is- ", cokePrice=0);
+}else{
+    console.log("Coke price is- ", cokePrice);
+}
