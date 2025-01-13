18cc6a1009ca73e3a4f2a3bffcd3ad1a87034b99
mahidulkabir <mahidulkabir8@gmail.com>
2025-01-13 23:48:27 +0600

    added ternary operation

---
+++ ternary-numbers.js
@@ -0,0 +1,21 @@
+// ternary numbers and ternary opertator 
+
+// by simple if - else statement 
+
+let num1 = 55;
+let num2 = 34;
+let result;
+
+// if (num1 > num2){
+//     result = num1*2;
+// } else{
+//     result = num1 + num2 ;
+// }
+
+
+
+// same thing is done by ternary operator 
+result = num1 > num2 ?  num1*2 : num1 + num2;
+console.log("first no. is ", num1);
+console.log("second no. is ", num2);
+console.log("Resul is ", result);
